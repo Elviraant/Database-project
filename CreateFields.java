aa73802e96c7c88e3edefe8a22142ed9472a4624
Andreadi Theodora <44115604+tandreadi@users.noreply.github.com>
2018-11-17 21:32:42 +0200

    Add files via upload
    Αυτό είναι κάτι πολύ πρόχειρο για να καταλάβετε πώς έχουμε εργαστεί. Προφανώς θα χωριστούν διαφορετικά οι λειτουργίες.
    Η βασική ιδέα είναι ότι το πρωτεύων κλειδί, αν υπάρχει, αποθηκεύεται σε ένα map,  της μορφής HashMap <String, Integer>, όπου String είναι το πρωτεύον κλειδί και Integer η θέση που αντιστοιχεί στα παράλληλα ArrayList που έχουν αποθηκευτεί τα υπόλοιπα δεδομένα.  Επίσης υπάρχει και ένα map της μορφής HashMap <String, ArrayList> που ενώνει τα ονόματα που δίνει ο χρήστης με τις λίστες που δημιουργούνται για τα fields.
    
    Το πρόγραμμα τρέχει αν απαντήσετε  "Y" στο αν θέλετε primary key και λειτουργεί για όσα πεδία θέλετε αλλά μόνο για δύο καταχωρήσεις σε κάθε πεδίο. Επειδή είναι πρόχειρο, προτείνω να το τρέξετε με λίγα πεδία, να διαβάζετε προσεχτικά τι ακριβώς ζητείται στα βοηθητικά print και να είστε συνεπής στην ορθογραφία των fields γιατί αυτή η έκδοση δεν έχει κανέναν έλεγχο για λάθος καταχώριση. Στο τέλος, έχουμε βάλει και μια μικρή αναζήτηση για καταλάβετε καλύτερα πως το έχουμε συνθέσει. 
    Οποιαδήποτε διόρθωση - πρόταση για τη δομή δεδομένων είναι δεκτή!!!!
    Φιλιά

---
+++ CreateFields.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class CreateFields  {
+
+
+	//For other attributes
+	public static ArrayList createStringField () {
+
+		ArrayList <String> nameofList = new ArrayList <String> ();
+		return nameofList;
+	}
+
+	public static ArrayList createIntegerField () {
+
+		ArrayList <Integer> nameofList = new ArrayList <Integer> ();
+		return nameofList;
+	}
+
+	public static ArrayList createDoubleField () {
+
+		ArrayList <Double> nameofList = new ArrayList <Double> ();
+		return nameofList;
+	}
+
+	public static ArrayList createBooleanField () {
+
+		ArrayList <Boolean> nameofList = new ArrayList <Boolean> ();
+		return nameofList;
+	}
+
+
+
+
+
+}
