4ac161a7bc650e2e8314224c078d90c79623e89b
Andreadi Theodora <44115604+tandreadi@users.noreply.github.com>
2018-11-20 20:26:45 +0200

    Add files via upload

--- TestCreateFields.java
+++ TestCreateFields.java
@@ -1,81 +1,73 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-//import org.junit.After;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestCreateFields {
 
-   // @Test
+
+	//@Before
+	//public void setUp() {
+
+	//	 HashMap<String, Integer> testMap = new HashMap<String, Integer>();
+
+	//}
+
+   @Test
 	public void testSetPrimaryKey(){
 
 		CreateFields.setPrimaryKey("ABC", 0);
-    	//assertEquals("failure - wrong set method", CreateFields.getPrimaryKey.size(), 1);
+    	Assert.assertTrue("failure - wrong set method", CreateFields.getPrimaryKey().get("ABC").equals(0));
 	}
 
-	//@Test
+
+	@Test
 	public void testGetPrimaryKey(){
 
-		CreateFields.setPrimaryKey("GRDFGFG", 1);
-		CreateFields.getPrimaryKey();
-    	//Assert.assertEquals("failure - wrong get method", list.size(), 1);
+		HashMap<String, Integer> testMap = new HashMap<String, Integer>();
+    	Assert.assertTrue("failure - wrong get method", CreateFields.getPrimaryKey().equals(testMap));
+
 
 	}
 
-	//@Test
-	private void testCreateStringField() {
+	@Test
+	public void testCreateStringField() {
 		ArrayList list1 = CreateFields.createStringField();
-		ArrayList<String> list2 = new ArrayList<String>();
-
-		if (list1.equals(list2)) {
-			System.out.println("Nice!");
-		} else {
-			System.out.println("Sth gone wrong!");
-		}
-
-		//Assert.assertArrayEquals(list2, list1);
+		ArrayList <String> list2 = new ArrayList<String>();
+       	Assert.assertTrue("failure - createStringField() method went wrong.",
+                list1.equals(list2));
 	}
 
-	//@Test
+	@Test
 	public void testCreateIntegerField() {
 		ArrayList list1 = CreateFields.createIntegerField();
-		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		ArrayList <Integer> list2 = new ArrayList<Integer>();
+		Assert.assertTrue("failure - createIntegerField() method went wrong.",
+			list1.equals(list2));
 
-		if (list1.equals(list2)) {
-			System.out.println("Nice!");
-		} else {
-			System.out.println("Sth gone wrong!");
-		}
 	}
 
-	//@Test
+	@Test
 	public void testCreateDoubleField() {
 		ArrayList list1 = CreateFields.createDoubleField();
-		ArrayList<Double> list2 = new ArrayList<Double>();
+		ArrayList <Double> list2 = new ArrayList<Double>();
 
-		if (list1.equals(list2)) {
-			System.out.println("Nice!");
-		} else {
-			System.out.println("Sth gone wrong!");
-		}
-
+       	Assert.assertTrue("failure - createDoubleField() method went wrong.",
+                list1.equals(list2));
 	}
 
-	//@Test
+	@Test
 	public void testCreateBooleanField() {
 		ArrayList list1 = CreateFields.createBooleanField();
-		ArrayList<Boolean> list2 = new ArrayList<Boolean>();
-
-		if (list1.equals(list2)) {
-			System.out.println("Nice!");
-		} else {
-			System.out.println("Sth gone wrong!");
-		}
-
+		ArrayList <Boolean> list2 = new ArrayList<Boolean>();
+		Assert.assertTrue("failure - createBooleanField() method went wrong.",
+                list1.equals(list2));
 	}
-//
+
+
 //   @After
 //    public void tearDown() {
 //        list = null;
