18778ae207b3f481877ac9420bc6eed8d2fe1f41
Andreadi Theodora <44115604+tandreadi@users.noreply.github.com>
2018-12-11 20:04:52 +0200

    TestTable, java doc
    java doc : containsName(String name) @Table class
    TestTable: testContainsName()

---
+++ TestTable.java
@@ -0,0 +1,34 @@
+//edw ginontai oi dokimes monadas gia kathe method tis klasis Table
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestTable {
+
+	private Table table = new Table("Test");
+	private Column column;
+	private FieldType type;
+	private final String firstElement = "NAME";
+   	private final String missingElement = "AGE";
+
+	@Before
+    public void setUp() {
+		table = new Table("Test");
+		type = new StringType();
+		column = new Column(firstElement, type, table);
+
+    }
+
+	@Test
+	public void testContainsName() {
+
+        Assert.assertEquals("failure - does not contain first element",
+                table.containsName(firstElement), 0);
+        Assert.assertEquals("failure - contains missing element",
+                table.containsName(missingElement), -1);
+
+	}
+
+}
