685b55547dca07dca1a6cbefb7788c4018585e75
MariaGkoulta <marygkoulta@gmail.com>
2018-12-15 21:57:48 +0200

    Exception handling in method choice

--- src/main/java/com/Databaseproject/databaseProject/Database.java
+++ src/main/java/com/Databaseproject/databaseProject/Database.java
@@ -3,6 +3,7 @@ Represents our Database
 */
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Database {
 	private static ArrayList<Table> tables = new ArrayList<Table>();
@@ -86,11 +87,31 @@ public class Database {
 	*@param end ending number of the range of options
 	*@return choice returns the final option
 	*/
+
+	/**
+	*checks if user gives an integer and handles Exception.
+	*@param return integer given by the user.
+	*/
+	public static int valid() {
+		int choice = 0;
+		boolean hasError = true;
+		while (hasError) {
+			try {
+				choice = Integer.parseInt(cs.next());
+				hasError = false;
+			} catch (Exception e) {
+				System.out.println("Please choose a valid option");
+				cs.reset();
+			}
+		}
+		return choice;
+	}
+
 	public static int choice(int start, int end) {
-		int choice = cs.nextInt();
+		int choice = valid();
 		while ((choice < start) || (choice > end)) {
 			System.out.println("Please chose a valid option");
-			choice = cs.nextInt();
+			choice = valid();
 		}
 		return choice;
 	}
