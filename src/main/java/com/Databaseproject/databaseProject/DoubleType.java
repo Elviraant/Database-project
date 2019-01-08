57260748dfed79e39e8d5ee1cbe9a7a84f42207b
kyriakos <kyriakosanton99@gmail.com>
2019-01-08 14:38:59 +0200

    checkstyle in DoubleType class

--- src/main/java/com/Databaseproject/databaseProject/DoubleType.java
+++ src/main/java/com/Databaseproject/databaseProject/DoubleType.java
@@ -1,39 +1,40 @@
 //package com.Databaseproject.databaseProject;
-import java.util.Scanner;
 import java.io.Serializable;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
- * Represent a Double Type of Database
+ * Represent a Double Type of Database.
  */
 public class DoubleType extends FieldType implements Serializable {
 
-	/**
-	 * Read a Double from keyboard
-	 * , catch InputMismatchException e and read again
-	 * @return Double Double, that has been read
-	 */
-	public Double getData() {
-		Scanner cs = new Scanner(System.in);
-		double data = 0;
-		while (true) {
-			try {
-				data = cs.nextDouble();
-				break;
-			} catch (InputMismatchException e) {
-				 System.out.println("You should insert a Double");
-				 cs.next();
-			}
-		}
-		return data;
+  /**
+   * Read a Double from keyboard
+   * , catch InputMismatchException e and read again.
+   * @return Double Double, that has been read.
+   */
+  
+  public Double getData() {
+    Scanner cs = new Scanner(System.in);
+    double data = 0;
+    while (true) {
+      try {
+        data = cs.nextDouble();
+        break;
+      } catch (InputMismatchException e) {
+        System.out.println("You should insert a Double");
+        cs.next();
+      }
+    }
+    return data;
+  }
 
-	}
-
-	/**
-	 * Return the type of the object
-	 * @return String
-	 */
-	public String toString() {
-		return "Type: Double";
-	}
+  /**
+   * Return the type of the object.
+   * @return String.
+   */
+ 
+  public String toString() {
+    return "Type: Double";
+  }
 }
