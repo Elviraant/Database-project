0acf75b341340832f5e5653ddbf2dc26e8641bd7
tandreadi <theodoraandreadi@gmail.com>
2019-01-08 21:15:56 +0200

    ; :(

--- src/test/java/com/Databaseproject/databaseProject/TestEnumeratedType.java
+++ src/test/java/com/Databaseproject/databaseProject/TestEnumeratedType.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestEnumeratedType {
 
   private EnumeratedType type;
-  private String missingValue = "Dog"
+  private String missingValue = "Dog";
 
   @Before
   public void setUp() {
